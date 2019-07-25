c7100db8cb9bbf4b55ec9a71579dc39e530acb33
KateDay <khummle@gmail.com>
2019-07-25 13:43:00 -0600

    rename file  fix spelling error of component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
 import './styles/App.css';
+import Background from './Background';
 import NewBeginnings from './NewBeginnings';
+import Projects from './Projects';
+import Resume from './Resume';
+import PersonalLife from './PersonalLife';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
 				<header>
-					<h2>Kate Day</h2>
+					<h1>Kate Day</h1>
 				</header>
 				<Background />
 				<NewBeginnings />
-				<Prjojects />
+				<Projects />
 				<Resume />
 				<PersonalLife />
-
-				
 			</div>
 		);
 	}
