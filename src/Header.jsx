b00df98f7c3544680dca080a2c75ff2fd56c3658
KateDay <khummle@gmail.com>
2019-07-30 15:23:44 -0600

    add return statement

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,17 +3,11 @@ import './styles/Header.css';
 
 class Header extends Component {
 	render() {
-		<header>
-			<div className="video-container">
-				<video autoplay loop muted>
-					<source
-						src="../build/Radio_Broadcasting_Board_In_Use.mp4"
-						type="video/mp4"
-					/>
-				</video>
-			</div>
-			<h1>Kate Day</h1>
-		</header>;
+		return (
+			<header>
+				<h1>Kate Day</h1>
+			</header>
+		);
 	}
 }
 
